9753ce1a1f6013f42be89f6f382843f74ae3cd6e
SowSkull <franciscorojas92@gmail.com>
2020-08-30 20:45:22 -0500

    configuration Travis

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -4,7 +4,6 @@ import getHash from "../utils/getHash.js"
 const Character = async () =>{
     let id = getHash();
     const character = await getData(id);
-    
     const view = `
         <div class="Characters-inner">
             <article class="Characters-card">
@@ -13,10 +12,10 @@ const Character = async () =>{
             </article>
             <article class="Characters-card">
             <h3> Episodes:<span> ${character.episode.length}</span></h3>
-            <h3> Status:  <span> ${character.status}  </span></h3> 
+            <h3> Status:  <span> ${character.status}  </span></h3>
             <h3> Species: <span> ${character.species} </span></h3>
-            <h3> Gender:  <span> ${character.gender}  </span></h3> 
-            <h3> Origin:  <span> ${character.origin.name} </span></h3> 
+            <h3> Gender:  <span> ${character.gender}  </span></h3>
+            <h3> Origin:  <span> ${character.origin.name} </span></h3>
             <h3> Last Location: <span> ${character.location.name}</span></h3>
             </article>
         </div>
